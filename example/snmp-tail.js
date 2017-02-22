51e93b61443a89b310e33d3b8c95dee903c09b4b
Shane Thompson <shane.thompson@aeontech.com.au>
2017-02-22 14:55:44 +0800

    Modified code styling

--- example/snmp-tail.js
+++ example/snmp-tail.js
@@ -1,34 +1,35 @@
 
 // Copyright 2013 Stephen Vickers
 
-var snmp = require ("../lib/");
+var snmp = require('../lib/');
 
 if (process.argv.length < 6) {
-	console.log ("usage: snmp-tail <target> <community> <version> <oid>");
-	process.exit (1);
+  console.log('usage: snmp-tail <target> <community> <version> <oid>');
+  process.exit(1);
 }
 
 var target = process.argv[2];
 var community = process.argv[3];
-var version = (process.argv[4] == "2c") ? snmp.Version2c : snmp.Version1;
+var version = (process.argv[4] == '2c') ? snmp.Version2c : snmp.Version1;
 
 var oid = process.argv[5];
 
-var session = snmp.createSession (target, community, {version: version});
+var session = snmp.createSession(target, community, { version: version });
 
-function poll () {
-	session.get ([oid], function (error, varbinds) {
-		if (error) {
-			console.error (error.toString ());
-		} else {
-			if (snmp.isVarbindError (varbinds[0]))
-				console.error (snmp.varbindError (varbinds[0]));
-			else
-				console.log (varbinds[0].value);
-		}
-	});
+function poll() {
+  session.get([oid], function (error, varbinds) {
+    if (error) {
+      console.error(error.toString());
+    } else {
+      if (snmp.isVarbindError(varbinds[0])) {
+        console.error(snmp.varbindError(varbinds[0]));
+      } else {
+        console.log(varbinds[0].value);
+      }
+    }
+  });
 }
 
-poll ();
+poll();
 
-setInterval (poll, 1000);
+setInterval(poll, 1000);
